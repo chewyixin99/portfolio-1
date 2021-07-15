b7d9d471aae863a1409d92de6d28a0a76497f8ce
Chew Yi Xin <76227132+chewyixin99@users.noreply.github.com>
2021-07-16 00:57:00 +0800

    Add files via upload
    Updated previous CSS styles
    Updated HTML templates to PHP
    Added basic comment system with reply feature for static showcase/project pages

---
+++ templates/showcase-11.php
@@ -0,0 +1,70 @@
+<?= require_once "./components/components.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Showcase 11</title>
+        <?= head(); ?>
+    </head>
+    <body>
+        <header>
+            <?= navbar(); ?>
+        </header>
+    
+
+        <section class="intro" id="home">
+            <h1 class="section__title section__title--intro">
+                The title of <strong>showcase 11</strong>
+            </h1>
+            <p class="section__subtitle section__subtitle--intro">A short subtitle</p>
+            <img src="../static/img/showcase_images/showcase-11.jpg"  alt="showcase-intro-img" class="intro__img">
+            
+        </section>
+
+        <div class="portfolio-item-individual">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe et amet tenetur! 
+                Fugit sequi optio corrupti fugiat ducimus consequatur incidunt?</p>
+            <img src="../static/img/showcase_images/showcase-11.jpg" alt="" class="intro__display__img">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe et amet tenetur! 
+                Fugit sequi optio corrupti fugiat ducimus consequatur incidunt?</p>
+            <p>Voluptatibus, soluta blanditiis! Incidunt ea unde itaque illo molestiae eligendi 
+                sint culpa nobis voluptas sapiente voluptate, magnam ipsum eius earum?</p>
+            
+            <a href="./index.php" class="btn_home">back to home</a>
+            
+        </div>
+
+        <div class="comments"></div>
+        
+        <!-- Footer -->
+        <?= footer() ?>
+
+        <script>
+            const comments_page_id = 11; // This number should be unique on every page
+            fetch("comments.php?page_id=" + comments_page_id).then(response => response.text()).then(data => {
+                document.querySelector(".comments").innerHTML = data;
+                document.querySelectorAll(".comments .write_comment_btn, .comments .reply_comment_btn").forEach(element => {
+                    element.onclick = event => {
+                        event.preventDefault();
+                        document.querySelectorAll(".comments .write_comment").forEach(element => element.style.display = 'none');
+                        document.querySelector("div[data-comment-id='" + element.getAttribute("data-comment-id") + "']").style.display = 'block';
+                        document.querySelector("div[data-comment-id='" + element.getAttribute("data-comment-id") + "'] input[name='name']").focus();
+                    };
+                });
+                document.querySelectorAll(".comments .write_comment form").forEach(element => {
+                    element.onsubmit = event => {
+                        event.preventDefault();
+                        fetch("comments.php?page_id=" + comments_page_id, {
+                            method: 'POST',
+                            body: new FormData(element)
+                        }).then(response => response.text()).then(data => {
+                            element.parentElement.innerHTML = data;
+                        });
+                    };
+                });
+            });
+        </script>
+
+
+    </body>
+</html>
